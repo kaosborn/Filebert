abd001d7df7a502d2add8949a3be210d58b1e897
kaosborn <kaosborn@users.noreply.github.com>
2018-10-14 17:01:11 -0700

    add Sha1xFormat, Sha256Format classes

---
+++ Source/Format/Types/Sha1xFormat.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using System.Text;
+using KaosCrypto;
+
+namespace KaosFormat
+{
+    public class Sha1xFormat : HashesContainer
+    {
+        public static string[] Names
+         => new string[] { "sha1x" };
+
+        public override string[] ValidNames
+         => Names;
+
+        public static Model CreateModel (Stream stream, byte[] hdr, string path)
+        {
+            if (path.ToLower().EndsWith(".sha1x"))
+                return new Model (stream, path);
+            return null;
+        }
+
+
+        public new class Model : HashesContainer.Model
+        {
+            public new readonly Sha1xFormat Data;
+
+            public Model (Stream stream, string path) : base (path, 20)
+            {
+                base._data = Data = new Sha1xFormat (this, stream, path);
+                ParseHashes();
+            }
+
+            public override void CalcHashes (Hashes hashFlags, Validations validationFlags)
+            {
+                if (Data.Issues.HasFatal)
+                    return;
+
+                if ((validationFlags & Validations.SHA1) != 0)
+                    ComputeContentHashes (new Sha1Hasher(), Hashes.MediaSHA1);
+
+                base.CalcHashes (hashFlags, validationFlags);
+            }
+        }
+
+
+        private Sha1xFormat (Model model, Stream stream, string path) : base (model, stream, path, Encoding.UTF8)
+         => Validation = Validations.SHA1;
+    }
+}
---
+++ Source/Format/Types/Sha256Format.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using KaosCrypto;
+
+namespace KaosFormat
+{
+    public class Sha256Format : HashesContainer
+    {
+        public static string[] Names
+         => new string[] { "sha256" };
+
+        public override string[] ValidNames
+         => Names;
+
+        public static Model CreateModel (Stream stream, byte[] hdr, string path)
+        {
+            if (path.ToLower().EndsWith(".sha256"))
+                return new Model (stream, path);
+            return null;
+        }
+
+
+        public new class Model : HashesContainer.Model
+        {
+            public new readonly Sha256Format Data;
+
+            public Model (Stream stream, string path) : base (path, 32)
+            {
+                base._data = Data = new Sha256Format (this, stream, path);
+                ParseHashes();
+            }
+
+            public override void CalcHashes (Hashes hashFlags, Validations validationFlags)
+            {
+                if (Data.Issues.HasFatal)
+                    return;
+
+                if ((validationFlags & Validations.SHA256) != 0)
+                    ComputeContentHashes (new Sha256Hasher());
+
+                base.CalcHashes (hashFlags, validationFlags);
+            }
+        }
+
+        private Sha256Format (Model model, Stream stream, string path) : base (model, stream, path)
+         => this.Validation = Validations.SHA256;
+    }
+}
