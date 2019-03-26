2b02141b8515d9253f8c16426012f6cd378f6459
kaosborn <kaosborn@users.noreply.github.com>
2019-03-25 23:11:44 -0700

    add UnknownFormat class

---
+++ Source/KaosFormat/Types/UnknownFormat.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using KaosIssue;
+
+namespace KaosFormat
+{
+    public class UnknownFormat : FormatBase
+    {
+        public static string[] SNames => new string[] { "*unknown*" };
+        public override string[] Names => SNames;
+
+        public new class Model : FormatBase.Model
+        {
+            public new readonly UnknownFormat Data;
+
+            public Model (Stream stream, string path)
+            {
+                base._data = Data = new UnknownFormat (this, stream, path);
+                IssueModel.Add ("Unknown extension.", Severity.Trivia);
+            }
+        }
+
+
+        public UnknownFormat (Model model, Stream stream, string path) : base (model, stream, path)
+        { }
+    }
+}
