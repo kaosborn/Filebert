c3da445be43d03524990451499103b06e5b92344
nobody <nobody@example.com>
2019-07-02 22:16:45 -0700

    add Test400 project

--- Test400/Properties/AssemblyInfo.cs
+++ Test400/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle ("TestFull")]
+[assembly: AssemblyTitle ("Test400")]
 [assembly: AssemblyProduct ("KaosDiags")]
 [assembly: ComVisible (false)]
-[assembly: Guid ("1f53b668-4c3a-4eb3-87c4-ff60cbf04b2c")]
+[assembly: Guid ("080ce209-e01a-4955-b364-289356e9f6e5")]
 [assembly: AssemblyVersion ("0.1.0.0")]
